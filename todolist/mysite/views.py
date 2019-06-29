4dc7c2cb3ce04ba099283f82156ff3e295fe38ae
kimdoyoung <develop_kimdoyoung@naver.com>
2019-06-29 01:37:40 -0700

    add process 거의 완료(return 메시지 구현이랑 save()작업만 하면 완료

--- todolist/mysite/views.py
+++ todolist/mysite/views.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import TodoList
 
 def index(request):
@@ -11,3 +14,19 @@ def index(request):
     }
     # index.html 파일에 context를 rendering
     return render(request,'mysite/index.html', context)
+
+def list_add(request):
+    # request.POST에 html에서 http 메소드 중 POST로 <form> 안의 데이터를 전달할 때
+    # POST 안에서 dictionary 객체로 존재한다.
+    content_name = request.POST['add_content']
+    pub_date = datetime.datetime.now()
+    exp_date = request.POST['exp_date']
+
+    # 할 일 : return으로 Django에서 lib로 제공하는 message 리턴
+    model_instance = TodoList.objects.create(contents=content_name,pub_date=pub_date,
+                                          exp_date=exp_date)
+    return HttpResponse(exp_date)
+
+# 장고 DB 활용 관련
+# https://brownbears.tistory.com/63
+# https://wayhome25.github.io/django/2017/04/01/django-ep9-crud/
